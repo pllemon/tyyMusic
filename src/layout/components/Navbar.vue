2aa9260c4799aa3eae387e8c656fae1a768b0fff
pllemon <37794820+pllemon@users.noreply.github.com>
2020-10-13 15:24:11 +0800

    1111

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -68,7 +68,7 @@ export default {
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
 
   .hamburger-container {
-    line-height: 46px;
+    line-height: 46px; 
     height: 100%;
     float: left;
     cursor: pointer;
